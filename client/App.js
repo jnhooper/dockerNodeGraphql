0aab36f03870c1c1ca3f49ba0e1b3cd0d52a07d5
jnhooper <nisse038@gmail.com>
2018-09-23 20:38:33 -0400

    remove bitches from hello world

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => <div>Hello World sup bitches</div>
+const App = () => <div>Hello World!</div>
 
 export default hot(module)(App)
 
