f3097d18cadfde3b5fa6ce8481a64a5aaf09a517
jnhooper <nisse038@gmail.com>
2018-09-23 20:37:31 -0400

    hot module reloading working!

---
+++ client/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => <div>Hello World sup bitches</div>
+
+export default hot(module)(App)
+
---
+++ webpack.config.client.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: './client/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 8080,
+    host: '0.0.0.0',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          // include: path.resolve(__dirname, 'client'),
+          include: './client',
+          cacheDirectory: true,
+          plugins: ['react-hot-loader/babel']
+        },
+      }
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
